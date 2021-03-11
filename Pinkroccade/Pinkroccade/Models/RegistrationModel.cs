17ce3699c360b698a20c517dcd512009a419fa27
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-03-11 10:45:15 +0100

    Model display changes
    displaynames veranderd van properties

--- Pinkroccade/Pinkroccade/Models/RegistrationModel.cs
+++ Pinkroccade/Pinkroccade/Models/RegistrationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,13 @@ namespace Pinkroccade.Models
 {
     public class RegistrationModel
     {
+        [DisplayName("First Name")]
         public string First_Name { get; set; }
+        [DisplayName("Last Name")]
         public string Last_Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        [DisplayName("E-Mail")]
         public string EMail { get; set; }
     }
 }
