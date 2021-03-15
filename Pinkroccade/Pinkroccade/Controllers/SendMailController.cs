bf89379c747f1229611498e779e8b4d3786809bc
Sander Aarts <sanderaarts19992@gmail.com>
2021-03-15 11:05:12 +0100

    MessageOption
    -Optie om berichten te versturen naar een mailadress.

---
+++ Pinkroccade/Pinkroccade/Controllers/SendMailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pinkroccade.Models;
+
+namespace Pinkroccade.Controllers
+{
+    public class SendMailController : Controller
+    {
+        // GET: SendMail
+        public ActionResult Index()
+        {
+            return View("SendMail");
+        }
+        public ActionResult Sender(MailModel mailModel)
+        {
+
+            Pinkroccade.MailHelper.smtpClient().Send(mailModel.fromMail, "Mailbox@Pinkroccade.nl", mailModel.subject, mailModel.body);
+            return View("SendMail");
+        }
+    }
+}
