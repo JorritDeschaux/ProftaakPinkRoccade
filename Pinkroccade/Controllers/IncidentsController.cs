aadf86b82265d22c23f3cff9cdfcc19a7437537d
Jorrit Deschaux <458871@student.fontys.nl>
2021-03-25 13:56:55 +0100

    Added Controller and View for Incidents. Also changed the Layout so the PinkRoccade ActionLink redirects to the index of IncidentsController.

---
+++ Pinkroccade/Controllers/IncidentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pinkroccade.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        // GET: Incidents
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
