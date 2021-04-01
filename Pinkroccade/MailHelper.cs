d186371afae4075d9e63cd70584ec7052729e001
Ryan Bours <ryan.bours@gmail.com>
2021-04-01 11:48:42 +0200

    Mailhelper added SendMail method

--- Pinkroccade/MailHelper.cs
+++ Pinkroccade/MailHelper.cs
@@ -5,19 +5,27 @@ using System.Web;
 using System.Net.Mail;
 using System.Net;
 
-namespace Pinkroccade
-{
-    public class MailHelper
-    {
-        public static SmtpClient smtpClient()
-        {
-            return new SmtpClient("smtp.mailtrap.io")
-            {
-                Port = 587,
-                Credentials = new NetworkCredential("836db70506df9b", "fff8be6a5c82be"),
-                EnableSsl = true,
-            };
-            //ryan "414dd7d23ac100", "322a95df45ae26"
+namespace Pinkroccade {
+    public class MailHelper {
+        static SmtpClient client = new SmtpClient("smtp.mailtrap.io") {
+            Port = 587,
+            Credentials = new NetworkCredential("836db70506df9b", "fff8be6a5c82be"),
+            EnableSsl = true,
+        };
+
+        public static void SendMail(string from, string[] to, string subject, string content) {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            to.ToList().ForEach(email => mail.To.Add(email));
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+            mail.Body = content;
+            client.Send(mail);
+        }
+
+        public static void SendMail(string from, string to, string subject, string content) {
+            string[] mails = { to };
+            SendMail(from, mails, subject, content);
         }
     }
 }
