5a3eb06dd8da03556f8adb5541a2a0a178165dc5
Sander Aarts <sanderaarts19992@gmail.com>
2021-04-01 14:25:26 +0200

    Created Login Page
    - Login creation

--- Pinkroccade/Controllers/LoginController.cs
+++ Pinkroccade/Controllers/LoginController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Pinkroccade.Models;
+using Pinkroccade.Classes;
 
 namespace Pinkroccade.Controllers
 {
     public class LoginController : Controller
     {
-        public ActionResult Login()
+        public ActionResult Index()
         {
-            return View();
+            return View("Login");
+        }
+
+        public ActionResult LoginAction(LoginModel loginModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                loginModel = Login.SelectUserData(loginModel);
+                if (loginModel.Unique_id != "" && loginModel.Unique_id != null)
+                {
+                    TempData["unique_id"] = loginModel.Unique_id;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View("Login");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+        public ActionResult NoAccount()
+        {
+            return View("~/Views/register/register.cshtml");
         }
     }
 }
+    
--- Pinkroccade/Models/LoginModel.cs
+++ Pinkroccade/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,16 @@ namespace Pinkroccade.Models
 {
     public class LoginModel
     {
-        public string Username { get; set; }
+        public string Unique_id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Enter Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
