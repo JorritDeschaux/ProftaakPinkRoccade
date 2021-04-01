cae0902d08e8b9f95a8ecc6e9226f342ec9ae958
Tom Derks <459530@student.fontys.nl>
2021-04-01 12:43:23 +0200

    Bugreport Pagina werkt, alleen fout bij link op navbar

---
+++ Pinkroccade/Controllers/BugReportController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pinkroccade.Models;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.IO;
+using System.Net.Mail;
+
+namespace Pinkroccade.Controllers
+{
+    public partial class BugReportController : Controller
+    {
+        // GET: BugReport
+        public ActionResult Index()
+        {
+            return View("BugReport");
+        }
+
+        public ActionResult Sender(MailModel mailModel)
+        {
+            //HttpPostedFileBase file = Request.Files[0];
+
+            //BinaryReader br = new BinaryReader(file.InputStream);
+            //Byte[] byteImage = br.ReadBytes((Int32)file.ContentLength);
+            //string Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+
+            string mailContent = $"{mailModel.body}";
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(mailModel.fromMail);
+            mail.To.Add(new MailAddress("Mailbox@Pinkroccade.nl"));
+            mail.Subject = mailModel.subject;
+            mail.IsBodyHtml = true;
+            mail.Body = mailContent;
+
+            MailHelper.smtpClient().Send(mail);
+            return View("BugReport");
+        }
+    }
+}
