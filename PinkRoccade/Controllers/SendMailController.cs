bdeba33274612c48c997d0331fbe9139eedfc770
Ryan Bours <ryan.bours@gmail.com>
2021-04-15 14:31:20 +0200

    Mail file upload fix

--- PinkRoccade/Controllers/SendMailController.cs
+++ PinkRoccade/Controllers/SendMailController.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using PinkRoccade.BS.Models;
+using PinkRoccade.BS.Classes;
 
 namespace PinkRoccade.Controllers
 {
@@ -13,19 +15,18 @@ namespace PinkRoccade.Controllers
             return View("SendMail");
         }
 
-        //public IActionResult Sender(MailModel mailModel)
-        //{
-        //    IFormFile file = Request.Form.Files[0];
+        public IActionResult Sender(MailModel mailModel) {
+            IFormFile file = Request.Form.Files[0];
 
-        //    BinaryReader br = new BinaryReader(file.InputStream);
-        //    Byte[] byteImage = br.ReadBytes((Int32)file.ContentLength);
-        //    string Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+            BinaryReader br = new BinaryReader(file.OpenReadStream());
+            Byte[] byteImage = br.ReadBytes((Int32) file.Length);
+            string Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
 
-        //    string mailContent = $"{mailModel.body} <img src='data:image/png;base64,{Base64string}' /> ";
+            string mailContent = $"{mailModel.body} <img src='data:image/png;base64,{Base64string}' /> ";
 
-        //    MailHelper.SendMail(mailModel.fromMail, "Mailbox@Pinkrocadde.nl", mailModel.subject, mailContent);
+            MailHelper.SendMail(mailModel.fromMail, "Mailbox@Pinkrocadde.nl", mailModel.subject, mailContent);
 
-        //    return View("SendMail");
-        //}
+            return View("SendMail");
+        }
     }
 }
