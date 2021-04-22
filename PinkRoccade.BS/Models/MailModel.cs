823c5c607b77fa27208618300d297e4cb77e7f03
onurinc <onur.i@hotmail.nl>
2021-04-22 10:53:20 +0200

    Added DataAnnotations for MailModel

--- PinkRoccade.BS/Models/MailModel.cs
+++ PinkRoccade.BS/Models/MailModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,18 @@ namespace PinkRoccade.BS.Models
 {
     public class MailModel
     {
+        [DisplayName("Onderwerp")]
+        [Required(ErrorMessage = "Vul hier het onderwerp over het probleem in")]
         public string subject { get; set; }
 
+        [DisplayName("E-mailadres")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Vul je e-mailadres hier in")]
         public string fromMail { get; set; }
 
+        [DisplayName("Informatie")]
+        [StringLength(500, ErrorMessage = "Je kan maximaal 500 karakters invoeren in dit veld")]
+        [Required(ErrorMessage = "Vul informatie over het probleem in")]
         public string body { get; set; }
 
         public string recipient = "sanderaarts19992@gmail.com";
