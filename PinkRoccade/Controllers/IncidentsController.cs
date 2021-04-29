0a06421e75d4783b45f8bd184117c10de90ec234
Jorrit Deschaux <458871@student.fontys.nl>
2021-04-29 12:50:34 +0200

    Small fix with model and html link

--- PinkRoccade/Controllers/IncidentsController.cs
+++ PinkRoccade/Controllers/IncidentsController.cs
@@ -23,33 +23,42 @@ namespace PinkRoccade.Controllers
         [HttpPost]
         public IActionResult CreateIncident(IncidentModel incidentModel)
         {
-            UserModel user = SessionHelper.GetObjectFromJson<UserModel>(HttpContext.Session, "_User");
+            if(ModelState.IsValid)
+			{
+                UserModel user = SessionHelper.GetObjectFromJson<UserModel>(HttpContext.Session, "_User");
 
-            string img_tag = null;
-            string Base64string = null;
-            if (Request.Form.Files.Count > 0)
-            {
-            IFormFile file = Request.Form.Files[0];
-                if (file.ContentType.Contains("img"))
+                string img_tag = null;
+                string Base64string = null;
+                if (Request.Form.Files.Count > 0)
                 {
-                    // idk ites
+                    IFormFile file = Request.Form.Files[0];
+                    if (file.ContentType.Contains("img"))
+                    {
+                        // idk ites
+                    }
+
+                    BinaryReader br = new BinaryReader(file.OpenReadStream());
+                    Byte[] byteImage = br.ReadBytes((Int32)file.Length);
+                    Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+
+                    img_tag = $"<img src='data:image/png;base64,{Base64string}'/>";
                 }
 
-            BinaryReader br = new BinaryReader(file.OpenReadStream());
-            Byte[] byteImage = br.ReadBytes((Int32)file.Length);
-            Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+                string mailContent = $"{incidentModel.Description} {img_tag ?? ""}";
+                string mailadres_sender = user.EMail;
+                MailHelper.SendMail((string)mailadres_sender, "Mailbox@Pinkrocadde.nl", incidentModel.Location, mailContent);
+                incidentModel.Img_Data = Base64string;
+                incidentModel.User_Id = user.Unique_id;
+                SaveIncident.Store_Incident(incidentModel);
 
-            img_tag = $"<img src='data:image/png;base64,{Base64string}'/>";
+                return RedirectToAction("Index");
             }
+			else
+			{
 
-            string mailContent = $"{incidentModel.Description} {img_tag??""}";
-            string mailadres_sender = user.EMail;
-            MailHelper.SendMail((string)mailadres_sender, "Mailbox@Pinkrocadde.nl", incidentModel.Location, mailContent);
-            incidentModel.Img_Data = Base64string;
-            incidentModel.User_Id = user.Unique_id;
-            SaveIncident.Store_Incident(incidentModel);
-
-            return RedirectToAction("Index");
+                return View("Index", incidentModel);
+			}
+         
         }
 
     }
