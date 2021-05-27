e0d0a4d6cf1d00c33b4b2fdda6a6f59399732028
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-05-27 13:52:51 +0200

    Send incident without login
    - able to send incidents without being logged in

--- PinkRoccade/Controllers/IncidentsController.cs
+++ PinkRoccade/Controllers/IncidentsController.cs
@@ -12,132 +12,134 @@ using PinkRoccade.BS.Models;
 
 namespace PinkRoccade.Controllers
 {
-	public class IncidentsController : Controller
-	{
-		public MySqlConnection GetSqlConnection()
-		{
-			MySqlConnection connectionString = new MySqlConnection("server = localhost; port = 3306; database = pinkroccade; user = root");
-			return connectionString;
-		}
+    public class IncidentsController : Controller
+    {
+        public MySqlConnection GetSqlConnection()
+        {
+            MySqlConnection connectionString = new MySqlConnection("server = localhost; port = 3306; database = pinkroccade; user = root");
+            return connectionString;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult Markers()
-		{
-			var data1 = GetMapMarkers();
-			return Ok(data1);
-		}
+        public IActionResult Markers()
+        {
+            var data1 = GetMapMarkers();
+            return Ok(data1);
+        }
 
-		private readonly IDNTCaptchaValidatorService _validatorService;
-		public IncidentsController(IDNTCaptchaValidatorService validatorService)
-		{
-			_validatorService = validatorService;
-		}
+        private readonly IDNTCaptchaValidatorService _validatorService;
+        public IncidentsController(IDNTCaptchaValidatorService validatorService)
+        {
+            _validatorService = validatorService;
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		[ValidateDNTCaptcha(
-		ErrorMessage = "Vul de juiste code in.",
-		CaptchaGeneratorLanguage = Language.English,
-		CaptchaGeneratorDisplayMode = DisplayMode.ShowDigits)]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateDNTCaptcha(
+        ErrorMessage = "Vul de juiste code in.",
+        CaptchaGeneratorLanguage = Language.English,
+        CaptchaGeneratorDisplayMode = DisplayMode.ShowDigits)]
 
-		[HttpPost]
-		public IActionResult CreateIncident(IncidentModel incidentModel)
-		{
-			if(ModelState.IsValid)
+        [HttpPost]
+        public IActionResult CreateIncident(IncidentModel incidentModel)
+        {
+            if (ModelState.IsValid)
             {
-				if (!_validatorService.HasRequestValidCaptchaEntry(Language.English, DisplayMode.ShowDigits))
-							{
-								this.ModelState.AddModelError("", "Vul de juiste code in.");
-								return View("Index");
-							}
-							UserModel user = SessionHelper.GetObjectFromJson<UserModel>(HttpContext.Session, "_User");
-							string img_tag = null;
-							string Base64string = null;
-							string mailadres_sender = incidentModel.Email;
-							if (Request.Form.Files.Count > 0)
-							{
-								IFormFile file = Request.Form.Files[0];
-								if (file.ContentType.Contains("img"))
-								{
-									// idk ites
-								}
-								BinaryReader br = new BinaryReader(file.OpenReadStream());
-								Byte[] byteImage = br.ReadBytes((Int32)file.Length);
-								Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
-								img_tag = $"<img src='data:image/png;base64,{Base64string}'/>";
-							}
-							string mailContent = $"{incidentModel.Description} {img_tag ?? ""}";
-							incidentModel.Img_Data = Base64string;
-							if (user == null)
-							{
-								MySqlConnection conn = GetSqlConnection();
-								MySqlCommand getUserData = new MySqlCommand("SELECT `id` FROM user WHERE email=@val1", conn);
-								getUserData.Parameters.AddWithValue("@val1", incidentModel.Email);
-								try
-								{
-									conn.Open();
-									getUserData.Prepare();
-									var executeString = getUserData.ExecuteReader();
-									while (executeString.Read())
-									{
-										var id = executeString.GetInt32(0);
-										incidentModel.User_Id = id;
-									}
-									conn.Close();
-								}
-								catch (Exception e)
-								{
-									Console.WriteLine("error: " + e.Message);
-								}
-							}
-							else
-							{
-								incidentModel.User_Id = user.Unique_id;
-								mailadres_sender = user.Email;
-							}
-							MailHelper.SendMail((string)mailadres_sender, "Mailbox@Pinkrocadde.nl", incidentModel.Location, mailContent);
-							SaveIncident.Store_Incident(incidentModel);
-							return RedirectToAction("Index");
+                if (!_validatorService.HasRequestValidCaptchaEntry(Language.English, DisplayMode.ShowDigits))
+                {
+                    this.ModelState.AddModelError("", "Vul de juiste code in.");
+                    return View("Index");
+                }
+                UserModel user = SessionHelper.GetObjectFromJson<UserModel>(HttpContext.Session, "_User");
+                string img_tag = null;
+                string Base64string = null;
+                string mailadres_sender = incidentModel.Email;
+                if (Request.Form.Files.Count > 0)
+                {
+                    IFormFile file = Request.Form.Files[0];
+                    if (file.ContentType.Contains("img"))
+                    {
+                        // idk ites
+                    }
+                    BinaryReader br = new BinaryReader(file.OpenReadStream());
+                    Byte[] byteImage = br.ReadBytes((Int32)file.Length);
+                    Base64string = Convert.ToBase64String(byteImage, 0, byteImage.Length);
+                    img_tag = $"<img src='data:image/png;base64,{Base64string}'/>";
+                }
+                string mailContent = $"{incidentModel.Description} {img_tag ?? ""}";
+                incidentModel.Img_Data = Base64string;
+                if (user == null)
+                {
+                    MySqlConnection conn = GetSqlConnection();
+                    MySqlCommand getUserData = new MySqlCommand("SELECT `id` FROM user WHERE email=@val1", conn);
+                    getUserData.Parameters.AddWithValue("@val1", incidentModel.Email);
+                    try
+                    {
+                        conn.Open();
+                        getUserData.Prepare();
+                        var executeString = getUserData.ExecuteReader();
+                        while (executeString.Read())
+                        {
+                            var id = executeString.GetInt32(0);
+                            if (id != 0)
+                            {
+                                incidentModel.User_Id = id;
+                            }
+                        }
+                        conn.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("error: " + e.Message);
+                    }
+                }
+                else
+                {
+                    incidentModel.User_Id = user.Unique_id;
+                    mailadres_sender = user.Email;
+                }
+                MailHelper.SendMail((string)mailadres_sender, "Mailbox@Pinkrocadde.nl", incidentModel.Location, mailContent);
+                SaveIncident.Store_Incident(incidentModel);
+                return RedirectToAction("Index");
             }
             else
             {
-				return RedirectToAction("Index", incidentModel);
+                return RedirectToAction("Index", incidentModel);
             }
-			
 
-		}
 
-		public IEnumerable<dynamic> GetMapMarkers()
-		{
-			MySqlConnection conn = GetSqlConnection();
-			MySqlCommand getMarkers = new MySqlCommand("SELECT * FROM alert", conn);
-			List<dynamic> markers = new List<dynamic>();
+        }
 
-			conn.Open();
+        public IEnumerable<dynamic> GetMapMarkers()
+        {
+            MySqlConnection conn = GetSqlConnection();
+            MySqlCommand getMarkers = new MySqlCommand("SELECT * FROM alert", conn);
+            List<dynamic> markers = new List<dynamic>();
 
-			var reader = getMarkers.ExecuteReader();
-			while (reader.Read())
-			{
-				markers.Add(new
-				{
-					Id = Convert.ToInt32(reader["id"]),
-					Status_Id = Convert.ToInt32(reader["status_id"]),
-					User_Id = Convert.ToInt32(reader["user_id"]),
-					Location = reader["location"].ToString(),
-					Latitude = reader["latitude"],
-					Longitude = reader["longitude"],
-					Description = reader["description"].ToString(),
-					Img_Data = reader["img_data"].ToString(),
-				}); ;
-			}
+            conn.Open();
 
-			conn.Close();
+            var reader = getMarkers.ExecuteReader();
+            while (reader.Read())
+            {
+                markers.Add(new
+                {
+                    Id = Convert.ToInt32(reader["id"]),
+                    Status_Id = Convert.ToInt32(reader["status_id"]),
+                    Location = reader["location"].ToString(),
+                    Latitude = reader["latitude"],
+                    Longitude = reader["longitude"],
+                    Description = reader["description"].ToString(),
+                    Img_Data = reader["img_data"].ToString(),
+                }); ;
+            }
 
-			return markers;
-		}
-	}
+            conn.Close();
+
+            return markers;
+        }
+    }
 }
