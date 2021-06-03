efc6db5ffd1df97f6d6cd8d5a58b012daefcac21
onurinc <Onur.i@hotmail.nl>
2021-06-03 10:36:38 +0200

    Implemented a new Contact page which fully works

---
+++ PinkRoccade.BS/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PinkRoccade.BS.Models
+{
+    public class ContactModel
+    {
+        [DisplayName("E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Vul je e-mailadres hier in")]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+
+        [DisplayName("Onderwerp")]
+        [Required(ErrorMessage = "Vul hier het onderwerp in")]
+        public string Subject { get; set; }
+
+        [DisplayName("Informatie")]
+        [Required(ErrorMessage = "Vul hier informatie in.")]
+        [StringLength(500, ErrorMessage = "Je kan maximaal 500 karakters invoeren in dit veld")]
+        [MaxLength(500)]
+        public string Body { get; set; }
+
+    }
+}
---
+++ PinkRoccade/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PinkRoccade.BS.Models;
+using System.Net.Mail;
+
+namespace PinkRoccade.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactModel contactModel)
+        {
+            string email = contactModel.Email;
+            string subject = contactModel.Subject;
+            string body = contactModel.Body;
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.To.Add(email);
+            mailMessage.Subject = subject;
+            mailMessage.From = new MailAddress("db3b3cdf8c-c3e97c@inbox.mailtrap.io");
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient("smtp.mailtrap.io");
+            smtp.Port = 2525;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("ad3f4fd6510412", "948abe13d2a8e6");
+            smtp.Send(mailMessage);
+            ViewBag.message = "Bedankt voor uw mail. We nemen zo spoedig contact met u op.";
+
+            return View("Contact");
+        }
+    }
+}
